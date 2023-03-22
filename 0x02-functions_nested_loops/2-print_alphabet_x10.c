451c99e638f1a8fde821814980874a7c3849a9a0
WilfredMutwiri <wilfredmutwiri20@gmail.com>
2023-03-22 05:25:37 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,9 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-	
 	int i;
 	
 	i = 0;
-
 	while (i < 10)
 	{
 		ch = 'a';
@@ -21,5 +19,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 		i++;	
-	}
-}
+	}}

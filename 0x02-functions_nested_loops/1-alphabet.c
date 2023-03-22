d8a2c24bd3d540f200868c46f6ee24957a116b80
WilfredMutwiri <wilfredmutwiri20@gmail.com>
2023-03-22 04:00:58 -0700

    letter

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,11 @@
 /**
  * print_alphabet -print all alphabet in lowercase
  */
- void print_alphabet(void)
+void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		_putchar(letter);
 	_putchar('\n');
 }
